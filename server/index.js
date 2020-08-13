496660e7d357607f3d0d22e590a7aab3979f86ea
Josh Nunez <joshjnunez09@gmail.com>
2020-08-13 12:23:25 -0500

    changes

--- server/index.js
+++ server/index.js
@@ -1,22 +1,36 @@
+require('dotenv').config();
+
+const { User } = require('./db/database.js');
+
 const express = require('express');
 const path = require('path');
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 const app = express();
-const port = process.env.PORT || 3000;
-const mockResponse = {
-  foo: 'bar',
-  bar: 'foo',
-};
+const { PORT } = process.env;
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
 
 app.use(express.static(DIST_DIR));
 
-app.get('/api', (req, res) => {
-  res.send(mockResponse);
+app.get('/grabUsers', (req, res) => {
+  console.log('send it');
+  User.findAll().then((user) => res.send(user));
 });
-app.get('/', (req, res) => {
-  res.sendFile(HTML_FILE);
+
+app.post('/addUsers', (req, res) => {
+  console.log(req.body.firstName);
+  User.create(req.body);
 });
-app.listen(port, function () {
-  console.log('App listening on port: ' + port);
+
+// app.get('/api', (req, res) => {
+//   res.send('hey it worked');
+// });
+// app.get('/', (req, res) => {
+//   res.sendFile(HTML_FILE);
+// });
+
+app.listen(PORT, function () {
+  console.log('App listening on port: ' + PORT);
 });
