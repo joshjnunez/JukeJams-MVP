a6a9c917e94f85f79328da6c6b4015012a1f2325
Josh Nunez <joshjnunez09@gmail.com>
2020-08-14 10:19:29 -0500

    .env back to local

--- server/db/database.js
+++ server/db/database.js
@@ -1,12 +1,54 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
+
 const { DB_NAME, DB_USER, DB_PASS, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
   dialect: 'mysql',
 });
+
+// let sequelize;
+// try {
+// console.log(process.env);
+// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+//   host: '130.211.115.84',
+//   port: '3306',
+//   dialect: 'mysql',
+//   // dialectOptions: {
+//   // socketPath: `/cloudsql/${DB_INSTANCE}`,
+//   // },
+// });
+// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+//   host: `/cloudsql/${process.env.DB_INSTANCE}`,
+//   dialect: 'mysql',
+//   dialectOptions: {
+//     socketPath: `/cloudsql/${DB_INSTANCE}`,
+//   },
+// });
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.info('INFO - Database connected.');
+//   })
+//   .catch((err) => {
+//     console.error('ERROR - Unable to connect to the database:', err);
+//   });
+// console.log(sequelize, 'connected');
+// return sequelize;
+// } catch (err) {
+//   console.error(err, sequelize, 'error db');
+// }
+
+// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+//   host: `/cloudsql/${process.env.DB_INSTANCE}`,
+//   dialect: 'mysql',
+//   dialectOptions: {
+//     socketPath: `/cloudsql/${DB_INSTANCE}`,
+//   },
+// });
 
 const User = sequelize.define('User', {
   firstName: Sequelize.STRING,
